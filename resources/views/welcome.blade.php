1cdab6b9f8e7bc16d52ce0ab455514aec90ee291
rahulsarkar272 <rahul@divyaltech.com>
2024-08-09 23:01:08 +0530

    update code for name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Vue 3 setup with laravel 10 and vite - WebJourney</title>
+	<title>Rahul </title>
   <!-- Favicons -->
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
